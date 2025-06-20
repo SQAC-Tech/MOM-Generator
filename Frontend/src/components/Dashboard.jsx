1f012b14f4527875ad979f5b0555705774ee4825
tusharikasuman <tusharika.suman6@gmail.com>
2025-06-21 01:14:48 +0530

    feat: Logo added

--- Frontend/src/components/Dashboard.jsx
+++ Frontend/src/components/Dashboard.jsx
@@ -29,6 +29,10 @@ const Dashboard = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-pink-300 via-purple-300 to-indigo-400 p-6 relative">
       <div className="max-w-6xl mx-auto">
+
+        <div className="flex justify-center mb-4">
+  <img src="/public/SQAClogo.png" alt="SQAC Logo" className="h-16 w-auto" />
+</div>
         <h1 className="text-4xl font-bold text-gray-800 mb-10 text-center">Dashboard</h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
