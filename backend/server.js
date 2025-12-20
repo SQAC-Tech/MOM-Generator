922f71f855e13d772ee3ed10cb56bfbe207065f9
Priyanshu Vasudev <priyanshuvasudev2005@gmail.com>
2025-12-20 18:48:47 +0530

    Fix: Cors Error

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:5173","https://mom-generator-eight.vercel.app/"],
+  origin: ["http://localhost:5173","https://mom-generator-eight.vercel.app"],
   credentials: true
 }));
 
